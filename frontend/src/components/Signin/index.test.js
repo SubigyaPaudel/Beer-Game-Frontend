56187c2a328875b20028731396388d382a18a668
SubigyaPaudel <subigyapaudel.123@gmail.com>
2021-04-25 18:22:21 +0200

    Made tests for components taking values from the store

--- frontend/src/components/Signin/index.test.js
+++ frontend/src/components/Signin/index.test.js
@@ -1,10 +1,34 @@
-import {render, screen} from '@testing-library/react';
-import {Login} from './index';
-import {Provider} from 'react-redux';
-import {Store} from '../../store'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Login from "./index";
+import { render, screen, getByText } from "@testing-library/react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { BrowserRouter as Router } from "react-router-dom";
+import "@testing-library/jest-dom/extend-expect";
 
-test('Properly renders sign-in-screen', () => {
-  render(<Login/>);
-  expect(screen.getByText('Sign in')).to
-})
+const mockStore = configureStore([]);
 
+describe("Login tests", () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      auth: {
+        isLoggedIn: false,
+        user: null,
+      },
+      message: "",
+    });
+  });
+
+  it("Login component renders successfully", () => {
+    render(
+      <Provider store={store}>
+        <Router>
+          <Login />
+        </Router>
+      </Provider>
+    );
+    expect(screen.getByText(/Sign in/i)).toBeInTheDocument();
+  });
+});
