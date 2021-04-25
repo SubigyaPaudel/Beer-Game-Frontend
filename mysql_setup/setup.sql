e174b31589beb61613f70029938cea634e74d1bb
Subigya_Paudel <56938247+SubigyaPaudel@users.noreply.github.com>
2021-04-25 22:34:29 +0200

    Update setup.sql

--- mysql_setup/setup.sql
+++ mysql_setup/setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE beergame4;
 CREATE DATABASE test_beergame4;
 
-CREATE USER "username"@"localhost" IDENTIFIED BY "password";
+CREATE USER "username"@"localhost" IDENTIFIED BY "Pass@123456789";
 
 USE beergame4;
 GRANT ALL PRIVILEGES ON beergame4.* TO "username"@"localhost";
